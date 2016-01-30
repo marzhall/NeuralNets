91bc7361db5f9b670ace8b6640b329a350b6d0f9
Marshall Conover <marzhall.o+github@gmail.com>
2016-01-30 11:41:52 -0500

    Change phrasing on the number of generations to go, and append the best old network to the mutated networks so as to not lose progress.

--- NeuralNets/GeneticTrainer.fs
+++ NeuralNets/GeneticTrainer.fs
@@ -6,8 +6,8 @@ module GeneticTrainer =
     let rec trainGenerations (numGenerations:int) (seedNetwork:Network)  (generationSize:int) (testInputs: 'a []) (learningRate:double) (trainingFunction : (('a []) -> Network.Network.Network -> 'b)) =
         let testGeneration =
             let generation = Array.Parallel.init generationSize (fun _ -> seedNetwork.mutatedClone(learningRate))
-            let best = Array.maxBy (trainingFunction testInputs) (generation)
-            printfn "Generation %A Done." numGenerations
+            let best = Array.maxBy (trainingFunction testInputs) (Array.append (generation) [|seedNetwork|])
+            printfn "%A generations to go." numGenerations
             best
         match numGenerations with
             | x  when x <= 0 -> testGeneration
